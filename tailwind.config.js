c2c1f9a555aac1173408a245b82907aeb0e181f6
vinhvuu224 <vinhvu224@gmail.com>
2022-03-21 13:07:57 -0700

    added header and navbar components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./pages/**/*.{html,js}',
   './components/**/*.{html,js}',],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 }
